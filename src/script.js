9140dc71bf289c3a77d5964be81ea1ccd34a121f
journeycruz-ping <journeycruz@pingidentity.com>
2022-05-13 11:09:38 -0500

    resize

--- src/script.js
+++ src/script.js
@@ -105,7 +105,8 @@ window.addEventListener('resize', () => {
  * Camera
  */
 // Base camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+const camera = new THREE.PerspectiveCamera(55, sizes.width / sizes.height, 0.1, 100)
+
 camera.position.x = 0
 camera.position.y = 0
 camera.position.z = 2
