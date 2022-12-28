ce1b332d23f47b20a7f776dbf1a29f5eadac0efc
journeycruz-ping <journeycruz@pingidentity.com>
2022-12-28 17:04:32 -0600

    wip - first working version

--- src/script.js
+++ src/script.js
@@ -1,188 +1,334 @@
 import './style.css'
 import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import * as dat from 'dat.gui'
+import * as Flickity from 'flickity'
 
-// Loading
-const textureLoader = new THREE.TextureLoader()
+{
+    let body,
+        mainContainer,
+        scene,
+        renderer,
+        camera,
+        cameraLookAt = new THREE.Vector3(0, 0, 0),
+        cameraTarget = new THREE.Vector3(0, 0, 800),
+        windowWidth,
+        windowHeight,
+        windowHalfWidth,
+        windowHalfHeight,
+        points,
+        mouseX = 0,
+        mouseY = 0,
+        gui,
+        stats,
+        contentElement,
+        colors = ['#F7A541', '#F45D4C', '#FA2E59', '#4783c3', '#9c6cb7'],
+        graphics,
+        currentGraphic = 0,
+        graphicCanvas,
+        gctx,
+        canvasWidth = 240,
+        canvasHeight = 240,
+        graphicPixels,
+        particles = [],
+        graphicOffsetX = canvasWidth / 2,
+        graphicOffsetY = canvasHeight / 2;
 
-const normalTexture = textureLoader.load('../textures/earthNormalMap.png')
-const baseColorMap = textureLoader.load('../textures/Metal_Rusted_010_basecolor.jpg')
-const aoMap1 = textureLoader.load('../textures/Metal_Rusted_010_ambientOcclusion.jpg')
-const heightMap = textureLoader.load('../textures/Metal_Rusted_010_height.png')
-const metalMap = textureLoader.load('../textures/Metal_Rusted_010_metallic.jpg')
-const normalTexture2 = textureLoader.load('../textures/Metal_Rusted_010_normal.jpg')
-const roughnessMap = textureLoader.load('../textures/Metal_Rusted_010_roughness.jpg')
+    // -----------------------
+    // Setup stage
+    // -----------------------
+    const initStage = () => {
+        body = document.querySelector('body');
+        mainContainer = document.querySelector('#main');
+        contentElement = document.querySelector('.intro-content');
 
-// Debug
-const gui = new dat.GUI()
+        setWindowSize();
 
-// Canvas
-const canvas = document.querySelector('canvas.webgl')
+        window.addEventListener('resize', onWindowResize, false);
+        window.addEventListener('mousemove', onMouseMove, false);
+    }
 
-// Scene
-const scene = new THREE.Scene()
+    // -----------------------
+    // Setup scene
+    // -----------------------
+    const initScene = () => {
+        scene = new THREE.Scene();
 
-// Objects
-const geometry = new THREE.SphereBufferGeometry(.5, 64, 64)
+        renderer = new THREE.WebGLRenderer({
+            alpha: true,
+            antialias: true
+        });
+        renderer.setPixelRatio(window.devicePixelRatio);
+        renderer.setSize(windowWidth, windowHeight);
+        mainContainer.appendChild(renderer.domElement);
 
-// Materials
+        scene.background = new THREE.Color(0xFFFFFF);
+    }
 
-const material = new THREE.MeshStandardMaterial()
-material.metalness = 0.2
-material.roughness = 0.7
-material.map = baseColorMap
-material.aoMap = aoMap1
-material.height = heightMap
-material.normalMap = normalTexture2
-material.roughnessMap = roughnessMap
-material.metalnessMap = metalMap
+    // -----------------------
+    // Setup camera
+    // -----------------------
+    const initCamera = () => {
+            const fieldOfView = 85;
+            const aspectRatio = windowWidth / windowHeight;
+            const nearPlane = 1;
+            const farPlane = 3000;
+            camera = new THREE.PerspectiveCamera(
+                fieldOfView,
+                aspectRatio,
+                nearPlane,
+                farPlane,
+            );
+            camera.position.z = 800;
+        }
+        // -----------------------
+        // Setup canvas
+        // -----------------------
+    const initCanvas = () => {
+        graphicCanvas = document.createElement('canvas');
+        graphicCanvas.width = canvasWidth;
+        graphicCanvas.height = canvasHeight;
+        gctx = graphicCanvas.getContext('2d');
+        graphics = document.querySelectorAll('.intro-cell > img');
+    }
 
-// Mesh
-const sphere = new THREE.Mesh(geometry, material)
-scene.add(sphere)
+    // -----------------------
+    // Setup light
+    // -----------------------
+    const initLights = () => {
+        const shadowLight = new THREE.DirectionalLight(0xffffff, 2);
+        shadowLight.position.set(20, 0, 10);
+        scene.add(shadowLight);
 
-// Lights
+        const light = new THREE.DirectionalLight(0xffffff, 1.5);
+        light.position.set(-20, 0, 20);
+        scene.add(light);
 
-const pointLight = new THREE.PointLight(0xffffff, 0.1)
-pointLight.position.x = 2
-pointLight.position.y = 3
-pointLight.position.z = 4
-pointLight.intensity = 2.14
-scene.add(pointLight)
+        const backLight = new THREE.DirectionalLight(0xffffff, 1);
+        backLight.position.set(0, 0, -20);
+        scene.add(backLight);
+    }
 
-const light1 = gui.addFolder('Light 1')
+    // -----------------------
+    // Setup particles
+    // -----------------------
 
-const pointLight2 = new THREE.PointLight(0xffffff, 2)
-pointLight2.intensity = 0
-pointLight2.position.set(-1.71, 1, -2.5)
-scene.add(pointLight2)
+    function Particle() {
+        this.vx = Math.random() * 0.05;
+        this.vy = Math.random() * 0.05;
+    }
 
-const light2 = gui.addFolder('Light 2')
+    Particle.prototype.init = function(i) {
+        const particle = new THREE.Object3D();
+        const geometryCore = new THREE.SphereGeometry(4, 4, 4);
+        const materialCore = new THREE.MeshBasicMaterial({
+            color: '#D73621'
+        });
 
-const pointLight3 = new THREE.PointLight(0xffffff, 2)
-pointLight3.intensity = 0
-pointLight3.position.set(6, 1, -9.5)
-scene.add(pointLight3)
+        const box = new THREE.Mesh(geometryCore, materialCore);
+        box.geometry.__dirtyVertices = true;
+        box.geometry.dynamic = false;
 
-const light3 = gui.addFolder('Light 3')
+        const pos = getGraphicPos(graphicPixels[i]);
+        particle.targetPosition = new THREE.Vector3(pos.x, pos.y, pos.z);
 
-light1.add(pointLight.position, 'y').min(-3).max(0.24).step(0.01)
-light1.add(pointLight.position, 'x').min(-6).max(6).step(0.01)
-light1.add(pointLight.position, 'z').min(-3).max(0.24).step(0.01)
-light1.add(pointLight, 'intensity').min(0).max(10).step(0.01)
-
-light2.add(pointLight2.position, 'y').min(-3).max(0.24).step(0.01)
-light2.add(pointLight2.position, 'x').min(-6).max(6).step(0.01)
-light2.add(pointLight2.position, 'z').min(-10).max(0.24).step(0.01)
-light2.add(pointLight2, 'intensity').min(0).max(10).step(0.01)
-
-light3.add(pointLight3.position, 'y').min(-3).max(0.24).step(0.01)
-light3.add(pointLight3.position, 'x').min(-6).max(6).step(0.01)
-light3.add(pointLight3.position, 'z').min(-10).max(0.24).step(0.01)
-light3.add(pointLight3, 'intensity').min(0).max(10).step(0.01)
-
-const pointLightHelper = new THREE.PointLightHelper(pointLight2, 1)
-const pointLightHelper2 = new THREE.PointLightHelper(pointLight3, 1)
-
-// scene.add(pointLightHelper)
-// scene.add(pointLightHelper2)
+        particle.position.set(windowWidth * 0.5, windowHeight * 0.5, -10 * Math.random() + 20);
+        randomPos(particle.position);
 
 
-/**
- * Sizes
- */
-const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
+        // for (var i = 0; i < box.geometry.vertices.length; i++) {
+        //     box.geometry.vertices[i].x += -2 + Math.random() * 4;
+        //     box.geometry.vertices[i].y += -2 + Math.random() * 4;
+        //     box.geometry.vertices[i].z += -2 + Math.random() * 4;
+        // }
+
+        particle.add(box);
+        this.particle = particle;
+    }
+
+    Particle.prototype.updateRotation = function() {
+        this.particle.rotation.x += this.vx;
+        this.particle.rotation.y += this.vy;
+    }
+
+    Particle.prototype.updatePosition = function() {
+        this.particle.position.lerp(this.particle.targetPosition, 0.13);
+    }
+
+    function updateParticles() {
+        for (var i = 0, l = particles.length; i < l; i++) {
+            particles[i].updateRotation();
+            particles[i].updatePosition();
+        }
+    }
+
+    const getGraphicPos = (pixel) => {
+        const posX = (pixel.x - graphicOffsetX - Math.random() * 4 - 2) * 3;
+        const posY = (pixel.y - graphicOffsetY - Math.random() * 4 - 2) * 3;
+        const posZ = -20 * Math.random() + 40;
+
+        return { x: posX, y: posY, z: posZ };
+    }
+
+    const setParticles = () => {
+        for (let i = 0; i < graphicPixels.length; i++) {
+            if (particles[i]) {
+                const pos = getGraphicPos(graphicPixels[i]);
+                particles[i].particle.targetPosition.x = pos.x;
+                particles[i].particle.targetPosition.y = pos.y;
+                particles[i].particle.targetPosition.z = pos.z;
+            } else {
+                const p = new Particle();
+                p.init(i);
+                scene.add(p.particle);
+                particles[i] = p;
+            }
+        }
+
+        for (let i = graphicPixels.length; i < particles.length; i++) {
+            randomPos(particles[i].particle.targetPosition, true);
+        }
+
+        console.log('Total Particles: ' + particles.length);
+    }
+
+    // -----------------------
+    // Random position
+    // -----------------------
+
+    function randomPos(vector, outFrame = false) {
+        const radius = outFrame ? (windowWidth * 5) : (windowWidth * -5);
+        const centerX = 0;
+        const centerY = 0;
+
+        // ensure that p(r) ~ r instead of p(r) ~ constant
+        const r = 875;
+        const angle = Math.random() * Math.PI * 2;
+
+        // compute desired coordinates
+        vector.x = centerX + r * Math.cos(angle);
+        vector.y = centerY + r * Math.sin(angle);
+        vector.z = Math.random() * 0.5;
+    }
+
+    // -----------------------
+    // Update canvas
+    // -----------------------
+
+    const updateGraphic = () => {
+        const img = graphics[currentGraphic];
+        gctx.drawImage(img, 0, 0, canvasWidth, canvasHeight);
+
+        const gData = gctx.getImageData(0, 0, canvasWidth, canvasHeight).data;
+        graphicPixels = [];
+
+        for (let i = gData.length; i >= 0; i -= 4) {
+            if (gData[i] == 0) {
+                const x = (i / 4) % canvasWidth;
+                const y = canvasHeight - Math.floor(Math.floor(i / canvasWidth) / 4);
+
+                if ((x && x % 2 == 0) && (y && y % 2 == 0)) {
+                    graphicPixels.push({
+                        x: x,
+                        y: y
+                    });
+                }
+            }
+        }
+
+        for (let i = 0; i < particles.length; i++) {
+            randomPos(particles[i].particle.targetPosition);
+        }
+
+        setTimeout(() => {
+            setParticles();
+        }, 500);
+    }
+
+    // -----------------------
+    // Setup background objects
+    // -----------------------
+
+    const initBgObjects = () => {
+        for (let i = 0; i < 40; i++) {
+            createBgObject(i);
+        }
+    }
+
+    const createBgObject = (i) => {
+        const geometry = new THREE.SphereGeometry(10, 6, 6);
+        const material = new THREE.MeshBasicMaterial({ color: 0xdddddd });
+        const sphere = new THREE.Mesh(geometry, material);
+        scene.add(sphere);
+        const x = Math.random() * windowWidth * 2 - windowWidth;
+        const y = Math.random() * windowHeight * 2 - windowHeight;
+        const z = Math.random() * -2000 - 200;
+        sphere.position.set(x, y, z);
+    }
+
+    // -----------------------
+    // Setup slider
+    // -----------------------
+
+    const initSlider = () => {
+        const elem = document.querySelector('.intro-carousel');
+
+        const flkty = new Flickity(elem, {
+            // options
+            cellAlign: 'center',
+            pageDots: false,
+            wrapAround: true,
+            resize: true
+        });
+
+        function listener() {
+            currentGraphic = flkty.selectedIndex;
+            updateGraphic();
+        }
+
+        flkty.on('select', listener);
+    }
+
+    setInterval(function() { document.querySelector(".next").click() }, 2000);
+
+    const onMouseMove = (event) => {
+        mouseX = (event.clientX - windowHalfWidth);
+        mouseY = (event.clientY - windowHalfHeight);
+        cameraTarget.x = (mouseX * -1) / 18;
+        cameraTarget.y = mouseY / 18;
+    }
+
+    const onWindowResize = () => {
+        setWindowSize();
+
+        camera.aspect = windowWidth / windowHeight;
+        camera.updateProjectionMatrix();
+        renderer.setSize(windowWidth, windowHeight);
+    }
+
+    const setWindowSize = () => {
+        windowWidth = window.innerWidth;
+        windowHeight = window.innerHeight;
+        windowHalfWidth = windowWidth / 2;
+        windowHalfHeight = windowHeight / 2;
+    }
+
+    const animate = () => {
+        requestAnimationFrame(animate);
+        updateParticles();
+        camera.position.lerp(cameraTarget, 0.2);
+        camera.lookAt(cameraLookAt);
+        render();
+    }
+
+    const render = () => {
+        renderer.render(scene, camera);
+    }
+
+    initStage();
+    initScene();
+    initCanvas();
+    initCamera();
+    initSlider();
+    // initBgObjects();
+    updateGraphic();
+    animate();
 }
-
-window.addEventListener('resize', () => {
-    // Update sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
-
-    // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
-
-    // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-})
-
-/**
- * Camera
- */
-// Base camera
-const camera = new THREE.PerspectiveCamera(55, sizes.width / sizes.height, 0.1, 100)
-
-camera.position.x = 0
-camera.position.y = 0
-camera.position.z = 2
-scene.add(camera)
-
-// Controls
-// const controls = new OrbitControls(camera, canvas)
-// controls.enableDamping = true
-
-/**
- * Renderer
- */
-const renderer = new THREE.WebGLRenderer({
-    canvas: canvas,
-    alpha: true
-})
-renderer.setSize(sizes.width, sizes.height)
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-
-/**
- * Animate
- */
-
-document.addEventListener('mousemove', onDocumentMouseMove)
-
-let mouseX = 0
-let mouseY = 0
-
-let targetX = 0
-let targetY = 0
-
-const windowHalfX = window.innerWidth / 2;
-const windowHalfY = window.innerHeight / 2;
-
-function onDocumentMouseMove(event) {
-    mouseX = (event.clientX - windowHalfX)
-    mouseY = (event.clientY - windowHalfY)
-}
-
-window.addEventListener('scroll', updateSphere)
-
-function updateSphere(event) {
-    sphere.position.y = window.scrollY * .001
-}
-
-const clock = new THREE.Clock()
-
-const tick = () => {
-    targetX = mouseX * .001
-    targetY = mouseY * .001
-
-    const elapsedTime = clock.getElapsedTime()
-
-    // Update objects
-    sphere.rotation.y = .5 * elapsedTime
-    sphere.rotation.y += .5 * (targetX - sphere.rotation.y)
-    sphere.rotation.x += .5 * (targetY - sphere.rotation.x)
-    sphere.position.z += .5 * (targetY - sphere.rotation.x)
-        // Update Orbital Controls
-        // controls.update()
-
-    // Render
-    renderer.render(scene, camera)
-
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
-}
-
-tick()
